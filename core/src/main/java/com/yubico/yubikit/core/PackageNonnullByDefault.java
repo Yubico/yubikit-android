d3256c7f7988f142289dd4db27bdf3930453ad23
Adam Velebil <adam.velebil@yubico.com>
2025-08-21 13:38:26 +0200

    revive file, deprecate instead

---
+++ core/src/main/java/com/yubico/yubikit/core/PackageNonnullByDefault.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020-2025 Yubico.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yubico.yubikit.core;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.annotation.meta.TypeQualifierDefault;
+import org.jspecify.annotations.NullUnmarked;
+
+/**
+ * Annotation used in package-info.java to indicate that all fields, methods, and parameters are
+ * Nonnull by default.
+ *
+ * <p>Deprecated, the SDK is now using @org.jspecify.annotations.NullMarked for packages
+ */
+@Documented
+@Deprecated
+@NullUnmarked
+@TypeQualifierDefault({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PackageNonnullByDefault {}
