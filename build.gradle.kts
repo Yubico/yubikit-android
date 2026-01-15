80eed2ded135a1d3c258d9e8d8e21e7816fddf13
Adam Velebil <adam.velebil@yubico.com>
2026-01-15 11:21:54 +0100

    version 3.0.0_3-fa

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ allprojects {
 }
 
 subprojects {
-    version = "3.0.0_2-fa"
+    version = "3.0.0_3-fa"
     tasks.withType<Javadoc>().configureEach {
         (options as? StandardJavadocDocletOptions)?.addStringOption(
             "Xdoclint:all,-missing",
